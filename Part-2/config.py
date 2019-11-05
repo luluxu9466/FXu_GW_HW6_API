eb79514616d269c8f80b21a76d08f21a952925b7
Fangyi Xu <fxu@caltech.edu>
2019-11-05 13:32:38 -0500

    part 2

---
+++ Part-2/config.py
@@ -0,0 +1,4 @@
+api_key = "89H5kMpwXb76oFKExRkgLREM7"
+api_secret_key = "Bx5J21RntglxoTl8tGG5Bp94HYwiZOYJ63mWedzwpueVfrWTHo"
+access_token = "1186753125610340357-VGa4tqxv3ch2cbUGyKWpm3VnHiRhGm"
+access_token_secret = "9MVDQL7Qf8wByj8fHt4jYTBXX1bKDvJpKn9ZjkRadxS4Y"
